a280da7387c760385c8c9fd262cf126df494c932
Donal971 <dominik.nalewaja@icloud.com>
2021-11-29 10:59:20 +0100

    Add Backendfiles for 3 UseCases

---
+++ Backend/login.php
@@ -0,0 +1,15 @@
+<?php
+    header('Content-Type: application/json');
+    $pdo = new PDO('mysql:host=localhost;dbname=app_db','root','');
+
+    $username = $_GET['Username'];
+    $password = $_GET['Password'];
+    $statement = $pdo->prepare("SELECT * FROM users WHERE Username = '".$username."'");
+    $statement->execute();
+    $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+    if ($data !== false && $data[0]['passwort'] == $password){
+        echo json_encode($data);
+    } else{
+        header('HTTP/1.1 404 Not found');
+    }
+?>
